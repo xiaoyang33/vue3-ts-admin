a0c7635e3d97d31f5fc8fbfa7eb8d423120e6173
yangwenlong <81355146@qq.com>
2021-09-11 17:01:05 +0800

    init-project

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+
+const path = require('path')
+
+const resolve = dir => path.join(__dirname,dir)
+
+module.exports = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+        '@':resolve('src')
+      }
+    }
+  }
+}
